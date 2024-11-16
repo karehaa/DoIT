cdb90d8c9304801cb249102379bbc5a8f23c8693
tristan.alhabas@gmail.com <tristan.alhabas@gmail.com>
2024-11-16 12:33:54 +0700

    Background Color Change

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
+      title: "Do!T",
       home: Scaffold(
+        backgroundColor: Color.fromRGBO(45, 114, 143, 1),
         body: Center(
           child: Text("Do!T"),
         ),
